4170e8f502aea4c92fe4f2245cff77fb086859a4
1maginaryNumbers <104255911+1maginaryNumbers@users.noreply.github.com>
2023-12-03 23:17:35 +0700

    update banyak

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from './pages/Login.jsx';
-import TemplateLogin from './TemplateLogin.jsx';
-import Template from './Template.jsx';
-
+import Login from "./pages/Login.jsx";
+import TemplateLogin from "./TemplateLogin.jsx";
+import Template from "./Template.jsx";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import Home from "./pages/Home.jsx";
 
 const router = createBrowserRouter([
-  {
-    path:"/",
-    element: <TemplateLogin />,
-    children:[
-      {
-        path:"",
-        element:<Login />
-      },
-    ]
-  }
-])
+    {
+        path: "/",
+        element: <TemplateLogin />,
+        children: [
+            {
+                path: "",
+                element: <Login />,
+            },
+            {
+                path: "home",
+                element: <Home />,
+            },
+        ],
+    },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}>
-
-    </RouterProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <RouterProvider router={router}></RouterProvider>
+        </Provider>
+    </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import { data } from "autoprefixer";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function Home() {
+    // const temp = useSelector((state) => state.);
+
+    return (
+        <>
+            <div className="h-screen flex items-center justify-center bg-repeat w-full">
+                <div className="bg-neutral-800 bg-opacity-90 p-5 px-10 w-[100] text-neutral-200 rounded-xl">
+                    hai
+                </div>
+            </div>
+        </>
+    );
+}
