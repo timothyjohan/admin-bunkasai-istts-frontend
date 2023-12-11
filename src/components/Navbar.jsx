14af8ab811ad7b7416559794ba24a3d8cf5bae15
lthreen <timothyjohan11@gmail.com>
2023-12-11 19:29:42 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,14 +11,20 @@ export default function Navbar(){
                         {/* <img src="nippongo.png" className="w-20 h-20 object-contain" /> */}
                     </div>
                     <div className="flex items-center text-neutral-300">
-                        <Link to={"/"} className="mx-5 text-xl font-bold transition-all hover:scale-110">
+                        <Link to={"/home"} className="mx-5 text-xl font-bold transition-all hover:scale-110">
                             <button className=" ">HOME</button>
                         </Link>
-                        <Link to={"/tenant-conf"} className="mx-5 text-xl font-bold transition-all hover:scale-110">
+                        <Link  className="mx-5 text-xl font-bold transition-all hover:scale-110">
                             <button className=" ">TENANT</button>
                         </Link>
-                        <Link to={"/competition-select"} className="mx-5 text-xl font-bold transition-all hover:scale-110">
+                        <Link  className="mx-5 text-xl font-bold transition-all hover:scale-110">
                             <button className="">COMPETITION</button>
+                        </Link>
+                        <Link  className="mx-5 text-xl font-bold transition-all hover:scale-110">
+                            <button className="">GALLERY</button>
+                        </Link>
+                        <Link  className="mx-5 text-xl font-bold transition-all hover:scale-110">
+                            <button className="">FEEDBACK</button>
                         </Link>
 
                         
