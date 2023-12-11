bc930b416c4850160c1d39281be53fd7f5bb4217
timothy <timothyjohan11@gmail.com>
2023-12-11 23:34:45 +0700

    change status realtime

--- src/components/Tenant.jsx
+++ src/components/Tenant.jsx
@@ -1,13 +1,21 @@
+import axios from "axios"
+import { useState } from "react"
+
 export default function Tenant(props){
+    const [status, setStatus] = useState(props.status)
+    const changeStatus = async () =>{
+        axios.put(`${import.meta.env.VITE_API_URL}/api/tenants/${props.telp}`)
+        setStatus(!status)
+    }
     return(
         <>
-            <tr className={`border border-neutral-300 ${props.status ? 'bg-green-400 text-violet-500' : 'bg-violet-500 text-green-400'}`}>
+            <tr className={`border border-neutral-300 ${status ? 'bg-green-400 text-violet-500' : 'bg-violet-500 text-green-400'}`}>
                 <td className="border border-neutral-300 px-5">{props.nama_tenant}</td>
                 <td className="border border-neutral-300 px-5">{props.nama_cp}</td>
                 <td className="border border-neutral-300 px-5">{props.telp}</td>
                 <td className="border border-neutral-300 px-5">{props.alamat}</td>
-                <td className="border border-neutral-300 px-5">{props.status ? `Diterima` : `Pending`}</td>
-                <td className="border border-neutral-300 px-5"><button className={` my-2 px-3 rounded-lg ${props.status ? 'bg-violet-500 text-green-400' : 'bg-green-400 text-violet-500'}`}>{props.status ? 'Hold' : 'Terima'}</button></td>
+                <td className="border border-neutral-300 px-5">{status ? `Diterima` : `Pending`}</td>
+                <td className="border border-neutral-300 px-5"><button onClick={changeStatus} className={` my-2 px-3 rounded-lg ${status ? 'bg-violet-500 text-green-400' : 'bg-green-400 text-violet-500'}`}>{status ? 'Hold' : 'Terima'}</button></td>
             </tr>
         </>
     )
