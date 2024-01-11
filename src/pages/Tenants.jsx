826b18c398e14463636972c8d7f370869c1edf0d
AlvinOeij <alvinoeij@gmail.com>
2024-01-11 14:48:58 +0700

    pagination

--- src/pages/Tenants.jsx
+++ src/pages/Tenants.jsx
@@ -7,7 +7,8 @@ import Tenant from "../components/Tenant";
 export default function Tenants() {
     // Mendefinisikan state lokal `tenants` dengan nilai awal array kosong
     const [tenants, setTenants] = useState([]);
-
+    const [update, setUpdate] = useState(true);
+    const [jumlah, setJumlah] = useState(0);
     // Mendefinisikan fungsi asinkron `getTenants` untuk mendapatkan data tenant dari API
     const getTenants = async () => {
         const request = await axios.get(
@@ -16,15 +17,83 @@ export default function Tenants() {
         setTenants(request.data);
     };
 
+    // Mendefinisikan fungsi asinkron `changeStatus` untuk mengubah status tenant
+    const changeStatus = async (index) => {
+        // Melakukan request PUT ke API untuk mengubah status tenant
+        axios.put(`${import.meta.env.VITE_API_URL}/api/tenants/${tenants[index].telp}`);
+
+        setTenants("")
+    };
+
     // Menggunakan `useEffect` untuk memanggil `getTenants` saat komponen dipasang
     useEffect(() => {
         getTenants();
-    }, []);
+    }, [!tenants]);
+
+    let arr=[]
+    
+    const prev = ()=>{
+        if(jumlah-3<0){
+            setJumlah(0)
+        }else{
+            setJumlah(jumlah-3)
+        }
+    }
+    const next = ()=>{
+        if(tenants.length<jumlah+3){
+            setJumlah(tenants.length-3)
+        }else{
+            setJumlah(jumlah+3)
+        }
+    }
+    
+    const page = 1;
+
+    if(tenants!=''){
+        for (let index = jumlah; index < jumlah+3; index++) {
+            if(index<tenants.length){
+                arr.push(
+                    <tr key={tenants[index].nama_cp}
+                        className={`border border-neutral-300 ${
+                            tenants[index].status
+                                ? "bg-green-400 text-violet-500"
+                                : "bg-violet-500 text-green-400"
+                        }`}
+                    >
+                    <td className="border border-neutral-300 px-5">
+                        {tenants[index].nama_tenant}
+                    </td>
+                    <td className="border border-neutral-300 px-5">
+                        {tenants[index].nama_cp}
+                    </td>
+                    <td className="border border-neutral-300 px-5">{tenants[index].telp}</td>
+                    <td className="border border-neutral-300 px-5">
+                        {tenants[index].alamat}
+                    </td>
+                    <td className="border border-neutral-300 px-5">
+                        {tenants[index].status ? `Diterima` : `Pending`}
+                    </td>
+                    {/* Membuat sel tabel dengan tombol untuk mengubah `status` */}
+                    <td className="border border-neutral-300 px-5">
+                        <button
+                            onClick={()=>changeStatus(index)}
+                            className={` my-2 px-3 rounded-lg ${
+                                tenants[index].status
+                                    ? "bg-violet-500 text-green-400"
+                                    : "bg-green-400 text-violet-500"
+                            }`}
+                        >
+                            {tenants[index].status ? "Hold" : "Terima"}
+                        </button>
+                    </td>
+                </tr>)
+            }
+        }
+    }
 
     // Mengembalikan JSX yang mendefinisikan tampilan komponen
     return (
         <>
-            
             <div className="min-h-screen min-w-screen flex items-center justify-center text-neutral-200">
                 <div className="bg-neutral-800 p-10 bg-opacity-75 rounded-xl">
                     <h1 className="text-2xl font-bold mb-5">TENANTS</h1>
@@ -52,11 +121,14 @@ export default function Tenants() {
                             </tr>
                         </thead>
                         <tbody>
-                            {tenants.map((element, key) => {
-                                return <Tenant key={key} {...element} />;
-                            })}
+                            {arr}
                         </tbody>
                     </table>
+                    <div className="mx-auto w-fit flex">
+                        <button onClick={prev}>prev</button>
+                        <div className="w-80 text-center"></div>
+                        <button onClick={next}>next</button>
+                    </div>
                 </div>
             </div>
         </>
