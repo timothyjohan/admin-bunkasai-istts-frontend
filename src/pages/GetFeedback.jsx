019e2a755c00bd3ad379d1a576969fca17eb6fa8
AlvinOeij <alvinoeij@gmail.com>
2024-01-15 09:04:59 +0700

    feedback pagination

--- src/pages/GetFeedback.jsx
+++ src/pages/GetFeedback.jsx
@@ -10,6 +10,8 @@ import { Outlet } from "react-router-dom";
 export default function GetFeedback() {
     // Mendefinisikan state lokal `feedbacks` dengan nilai awal array kosong
     const [feedbacks, setFeedbacks] = useState([]);
+    const [page, setPage] = useState(1);
+    const [jumlah, setJumlah] = useState(0);
 
     // Mendefinisikan fungsi asinkron `getFeedbacks` untuk mendapatkan data feedback dari API
     const getFeedbacks = async () => {
@@ -22,7 +24,51 @@ export default function GetFeedback() {
     // Menggunakan `useEffect` untuk memanggil `getFeedbacks` saat komponen dipasang
     useEffect(() => {
         getFeedbacks();
-    }, []);
+    }, [!feedbacks]);
+
+    let arr = []
+
+    const prev = ()=>{
+        if(jumlah-3<0){
+            setJumlah(0)
+        }else{
+            setJumlah(jumlah-3)
+        }
+    }
+    const next = ()=>{
+        if(tenants.length<=jumlah+3){
+            setJumlah(tenants.length-3)
+        }else{
+            setJumlah(jumlah+3)
+        }
+    }
+    
+
+    if(feedbacks!=''){
+        for (let index = jumlah; index < jumlah+3; index++) {
+            if(index<feedbacks.length){
+                arr.push(
+                    <tr className={`border border-neutral-300`}>
+                        {/* Membuat sel tabel untuk `kritik`, `saran`, `pesan`, dan `kesan` */}
+                        <td className="border border-neutral-300 px-5">
+                            {feedbacks[index].kritik}
+                        </td>
+                        <td className="border border-neutral-300 px-5">
+                            {feedbacks[index].saran}
+                        </td>
+                        <td className="border border-neutral-300 px-5">
+                            {feedbacks[index].pesan}
+                        </td>
+                        <td className="border border-neutral-300 px-5">
+                            {feedbacks[index].kesan}
+                        </td>
+                    </tr>
+                )
+            }
+        }
+    }
+
+
 
     // Mengembalikan JSX yang mendefinisikan tampilan komponen
     return (
@@ -50,12 +96,14 @@ export default function GetFeedback() {
                             </tr>
                         </thead>
                         <tbody>
-                            {/* Menggunakan `map` untuk membuat komponen `Feedback` untuk setiap item dalam `feedbacks` */}
-                            {feedbacks.map((element, key) => {
-                                return <Feedback key={key} {...element} />;
-                            })}
+                            {arr}
                         </tbody>
                     </table>
+                    <div className="mx-auto w-fit flex">
+                        <button onClick={prev}>prev</button>
+                        <div className="w-80 text-center">{(jumlah/3)+1}/{Math.ceil(page/3)}</div>
+                        <button onClick={next}>next</button>
+                    </div>
                 </div>
             </div>
         </>
